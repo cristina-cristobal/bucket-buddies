28e7b410c6c1a94f2c0d886da420d8a344d7d51d
github username <github email address>
2019-05-06 17:20:53 -0400

    added register, and login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(username: params[:username].downcase)
+    if @user
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash["notice"] = "Sorry, that user was not found"
+      render :new
+    end
+  end
+
+  # private
+  #
+  # def find_user
+  #   @user = User.find(params(:user_id))
+  # end
 end
